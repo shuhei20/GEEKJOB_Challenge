2f4c5e630cc08196b623ba4aa476ed7138229a6d
染川脩平 <syu.20.ver2.31@gmail.com>
2018-03-03 03:56:19 +0900

    標準クラス課題15-5課題提出します

---
+++ CampChallenge_java/015.標準クラス/Byte.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mojiretsu;
+
+/**
+ *
+ * @author shuhei01
+ */
+public class Byte {
+    public static void main(String[] args) {
+        
+        String name = "染川脩平";
+        
+        System.out.println("文字数は" + name.length() + "です");
+        System.out.println("バイト数は" + name.getBytes().length + "byteです");
+        
+    }
+    
+}
