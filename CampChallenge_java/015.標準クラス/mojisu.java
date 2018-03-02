2a8c21f3599aa8aa83307b00fdfac4fc9ee8a799
染川脩平 <syu.20.ver2.31@gmail.com>
2018-03-03 03:57:17 +0900

    標準クラス課題15-6課題提出します

---
+++ CampChallenge_java/015.標準クラス/mojisu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mojiretsu;
+
+/**
+ *
+ * @author shuhei01
+ */
+public class mojisu {
+    public static void main(String[] args) {
+        
+        String mail = "syu.20.ver2.31@gmail.com";
+        
+        System.out.println(mail.substring(14));
+        
+    }
+    
+}
