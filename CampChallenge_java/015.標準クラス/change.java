5b1b35c58008b71a80171e287c3ba8f743bebe4d
染川脩平 <syu.20.ver2.31@gmail.com>
2018-03-03 03:58:27 +0900

    標準クラス課題15-7課題提出します

---
+++ CampChallenge_java/015.標準クラス/change.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mojiretsu;
+
+/**
+ *
+ * @author shuhei01
+ */
+public class change {
+    
+    public static void main(String[] args) {
+        
+        String a = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        
+        String b = a.replace("U","う");
+        System.out.println(b.replace("I","い"));
+        
+    }
+    
+}
