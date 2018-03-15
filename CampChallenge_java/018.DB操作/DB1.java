d5150e9b74897a3bab2e35b4763f4ae40311ca68
染川脩平 <syu.20.ver2.31@gmail.com>
2018-03-15 17:22:10 +0900

    DB操作課題18-1課題提出します

---
+++ CampChallenge_java/018.DB操作/DB1.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataBase;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+/**
+ *
+ * @author shuhei01
+ */
+public class DB1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    Connection db_con = null;
+    PreparedStatement db_st = null;
+    ResultSet db_data = null;
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        
+        
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db","root","");
+            db_st = db_con.prepareStatement("SELECT * FROM profiles");
+            
+            db_data = db_st.executeQuery();
+            while(db_data.next()) {
+                out.print("名前：" + db_data.getString("name") + "<br>");
+            }
+            db_data.close();
+            db_st.close();
+            db_con.close();
+
+        } catch (SQLException se_con) {
+
+            System.out.println("SQL接続失敗" + se_con.getMessage());
+
+        } catch (Exception e_con) {
+
+            System.out.println("接続失敗" + e_con.getMessage());
+
+        }
+    }
+}
+
+ 
